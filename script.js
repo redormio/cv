073a37c28ce7d1d4ce4a1c323f54f8a5f24e9867
Anastasia <63669989+redormio@users.noreply.github.com>
2023-09-25 12:38:44 +0500

    Update script.js

--- script.js
+++ script.js
@@ -2,12 +2,15 @@ document.addEventListener("DOMContentLoaded", function () {
     const showMoreButton = document.getElementById("show-more-button");
     const hobbiesGrid = document.getElementById("hobbies-grid");
 
+    // Initially hide the hobbies grid
+    hobbiesGrid.style.display = "none";
+
     showMoreButton.addEventListener("click", function () {
-        if (hobbiesGrid.classList.contains("hidden")) {
-            hobbiesGrid.classList.remove("hidden");
+        if (hobbiesGrid.style.display === "none") {
+            hobbiesGrid.style.display = "grid";
             showMoreButton.textContent = "Show Less";
         } else {
-            hobbiesGrid.classList.add("hidden");
+            hobbiesGrid.style.display = "none";
             showMoreButton.textContent = "Show More";
         }
     });
